3acd98f8805f26740e511cdc0e2f5e564525c33f
Pierre-Cathe <pierre.cathe@gmail.com>
2019-05-21 22:07:21 +0200

    Add skeleton

---
+++ civs.js
@@ -0,0 +1,15 @@
+var data=[
+    {
+        "name": "Poland",
+        "leader": "Casimir III",
+    },
+    {
+        "name": "Venice",
+        "leader": "Enrico Dandolo",
+    },
+    {
+        "name":"France",
+        "leader":"Napoleon Bonaparte"
+    }
+];
+export data;
